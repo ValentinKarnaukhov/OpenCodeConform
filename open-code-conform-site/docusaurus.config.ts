55a55825fa09bba37d27f1f70d1e57b6bd5f9fd1
ValentinKarnaukhov <valentin.karnaukhovv@gmail.com>
2024-07-20 21:41:10 +0300

    OCC-4 hosted to pages
    Signed-off-by: ValentinKarnaukhov <valentin.karnaukhovv@gmail.com>

--- open-code-conform-site/docusaurus.config.ts
+++ open-code-conform-site/docusaurus.config.ts
@@ -8,15 +8,15 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://valentinkarnaukhov.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/OpenCodeConform/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'ValentinKarnaukhov', // Usually your GitHub org/user name.
+  projectName: 'OpenCodeConform', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
